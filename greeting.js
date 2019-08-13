e6b55afbd9158c18322c1db0807302d041ca19c3
jbj828 <53502444+jbj828@users.noreply.github.com>
2019-08-14 01:27:11 +0900

    cm

---
+++ greeting.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
